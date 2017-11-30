38b553491acdca4b4de15890425973336cc3ade1
Brendan Koral <bkoral@dealerinspire.com>
2017-11-30 16:50:20 -0600

    Add .travis folder and build script

---
+++ .travis/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Running deploy"
+
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+chmod 600 .travis/deploy_key # Allow read access to the private key
+ssh-add .travis/deploy_key # Add the private key to SSH
+
+
+
+# Skip this command if you don't need to execute any additional commands after deploying.
+ssh -o StrictHostKeyChecking=no koralbuild@$IP -p $PORT <<EOF
+  cd $DEPLOY_DIR
+  
+  if [ ! -d $DEPLOY_DIR ]; then
+    # clone the repo on to the server
+    echo "Cloning repo from github"
+    git clone $REPO $DEPLOY_DIR
+    cd $LIVE_DIR
+  else
+    # pull changes
+    echo "Pulling changes from github"
+    cd $LIVE_DIR
+    git pull
+  fi
+  
+  echo "INSTALLING NODE PACKAGES"
+  npm install
+
+  echo "UPDATING PACKAGES JUST TO BE SURE"
+  npm update
+
+  echo "RESTARTING BKORAL.IO SERVER"
+  pm2 restart bkoralio-server.js
+EOF

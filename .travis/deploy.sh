9311c7eb5faeac8351e3db1809c6f0396b2170e9
Brendan Koral <bkoral@dealerinspire.com>
2017-11-30 20:57:01 -0600

    Check if deploy dir is empty

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -12,14 +12,14 @@ ssh-add .travis/deploy_key # Add the private key to SSH
 ssh -o StrictHostKeyChecking=no koralbuild@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
   
-  if [ ! -d $DEPLOY_DIR ]; then
+  if [ ! "$(ls -A $DEPLOY_DIR)" ]; then
     # clone the repo on to the server
-    echo "Cloning repo from github"
+    echo "Cloning repo from github \n"
     git clone $REPO $DEPLOY_DIR
     cd $DEPLOY_DIR
   else
     # pull changes
-    echo "Pulling changes from github"
+    echo "Pulling changes from github \n"
     cd $DEPLOY_DIR
     git pull
   fi
